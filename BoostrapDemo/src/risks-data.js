800324ee7b4322e6ef77891f619bb09db9ec787e
Mohamed Soleit <mohamed.soleit@hotmail.com>
2022-09-17 18:01:03 +0200

    Add risks and shipments data and starting backend-infra

---
+++ BoostrapDemo/src/risks-data.js
@@ -0,0 +1,13 @@
+const risks = [
+    {
+        "id": 17,
+        "location": {
+            "lng": 1.3329,
+            "lat": 103.55614
+        },
+        "category": "WEATHER",
+    }
+
+];
+
+export default risks;
---
+++ BoostrapDemo/src/shipments-data.js
@@ -0,0 +1,11 @@
+const shipments = [
+    {
+        "warning_id": 17,
+        "port_destination_name": "Rotterdam",
+        "port_destination_land": "NL",
+        "empfaenger_ort": "VOLKETSWIL"
+    },
+
+];
+
+export default shipments;
